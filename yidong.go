e5fb8d42a0be2fb25240f340ccfc94c6a4a8e3e0
Alton00001 <94725833+Alton00001@users.noreply.github.com>
2024-04-02 09:56:57 +0800

    Add files via upload

---
+++ yidong.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
+)
+
+var (
+	windowSize int
+	queueNum   int
+)
+
+func modifyWindow(packet gopacket.Packet) {
+	ipLayer := packet.Layer(layers.LayerTypeIPv4)
+	if ipLayer == nil {
+		return
+	}
+
+	ip, _ := ipLayer.(*layers.IPv4)
+
+	tcpLayer := packet.Layer(layers.LayerTypeTCP)
+	if tcpLayer == nil {
+		return
+	}
+
+	tcp, _ := tcpLayer.(*layers.TCP)
+
+	if tcp.SYN {
+		tcp.Window = uint16(windowSize)
+		ip.FixChecksums()
+		tcp.SetNetworkLayerForChecksum(ip)
+	}
+}
+
+func main() {
+	flag.IntVar(&windowSize, "w", 4, "TCP Window Size")
+	flag.IntVar(&queueNum, "q", 0, "iptables Queue Num")
+	flag.Parse()
+
+	handle, err := pcap.OpenLive("eth0", 1600, true, pcap.BlockForever)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening device: %v\n", err)
+		os.Exit(1)
+	}
+	defer handle.Close()
+
+	if err := handle.SetBPFFilter(fmt.Sprintf("tcp port %d", queueNum)); err != nil {
+		fmt.Fprintf(os.Stderr, "Error setting BPF filter: %v\n", err)
+		os.Exit(1)
+	}
+
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	packetChan := packetSource.Packets()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		for {
+			select {
+			case packet := <-packetChan:
+				modifyWindow(packet)
+			case <-sig:
+				fmt.Println("Exiting...")
+				os.Exit(0)
+			}
+		}
+	}()
+
+	fmt.Println("Starting packet processing...")
+	<-sig
+}
